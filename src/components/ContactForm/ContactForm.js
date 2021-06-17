41cb2842c3d7da38e5d5f5c4822a323cb620d6e3
Darshan Shah <darshanshah7869@gmail.com>
2021-06-17 13:45:04 +0530

    test

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -8,18 +8,22 @@ function ContactForm() {
       <h2 className="contact-form__title">Leave a Message</h2>
       <div className="contact-form__body">
         <div className="contact-form__row">
-          <input
-            type="text"
-            placeholder="Name"
-            className="contact-form__name"
-          />
-          <input
-            type="email"
-            placeholder="Email"
-            className="contact-form__mail"
-          />
+          <div className="contact-form__name-box">
+            <input
+              type="text"
+              placeholder="Name"
+              className="contact-form__name"
+            />
+          </div>
+          <div className="contact-form__mail-box">
+            <input
+              type="email"
+              placeholder="Email"
+              className="contact-form__mail"
+            />
+          </div>
         </div>
-        <div className="contact-form__row">
+        <div className="contact-form__row--2">
           <textarea placeholder="Message" className="contact-form__message" />
         </div>
         <button className="contact-form__submit">Submit</button>
