54ca44b2bdbd0d0c1859974134af3c96f404d394
Darshan-Shah7869 <darshanshah7869@gmail.com>
2021-07-04 19:25:57 +0530

    Updated

--- src/components/HeroBox/HeroBox.js
+++ src/components/HeroBox/HeroBox.js
@@ -5,7 +5,7 @@ import './HeroBox.scss';
 function HeroBox() {
   return (
     <div className="hero-box">
-      <h1 className="hero-box__title">Lakshya 2021</h1>
+      <h1 className="hero-box__title">Lakshya Fest 2021</h1>
       <div className="hero-box__button-box">
         <button className="hero-box__button hero-box__button--1">
           Get Started
