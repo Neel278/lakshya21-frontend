c7965af9d1d151e46971ba9353cb30488a149ad9
Darshan-Shah7869 <darshanshah7869@gmail.com>
2021-07-07 13:27:04 +0530

    Date Added

--- src/components/HeroBox/HeroBox.js
+++ src/components/HeroBox/HeroBox.js
@@ -6,6 +6,7 @@ function HeroBox() {
   return (
     <div id="hero" className="hero-box">
       <h1 className="hero-box__title">Lakshya Fest 2021</h1>
+      <h2 className="hero-box__sub-title">03-08-2021 To 08-08-2021</h2>
       <div className="hero-box__button-box">
         <button className="hero-box__button hero-box__button--1">
           Get Started
