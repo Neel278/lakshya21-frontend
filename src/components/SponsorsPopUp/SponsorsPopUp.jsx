3f2828890ab9664d6e4685d28eb2e591acf4de1b
Om Raval <r.auckwell18@gmail.com>
2021-07-18 20:01:48 +0530

    Sponsors PopUp added

---
+++ src/components/SponsorsPopUp/SponsorsPopUp.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import PopUp from '../PopUp/PopUp';
+import './SponsorsPopUp.scss';
+
+function SponsorsPopUp(props) {
+  return (
+    <div className="sponsors-wrapper">
+      <div className="sponsored-by__logo-box">
+        <div className="sponsored-by__logo-item">
+          <img
+            src={props.source}
+            alt={props.sponsorAlt}
+            className="sponsored-by__logo"
+            onMouseOver={props.imgHover}
+          />
+        </div>
+      </div>
+      <PopUp
+        bg={props.SponsorsBanner}
+        paperHeight="50%"
+        bgColor="white"
+        open={props.openSponsors}
+        close={props.closeSponsors}
+      />
+    </div>
+  );
+}
+
+export default SponsorsPopUp;
